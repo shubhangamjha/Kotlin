6bcef352de841cea9885bd4b36b438b717fd492c
shubhangamjha <105533871+shubhangamjha@users.noreply.github.com>
2022-09-17 14:12:40 +0530

    first commit

---
+++ Unread message.kt
@@ -0,0 +1,13 @@
+/**
+ * This program displays the number of messages
+ * in the user's inbox.
+ */
+fun main() {
+    // Create a variable for the number of unread messages.
+    var count = 10
+    println("You have $count unread messages.")
+
+    // Decrease the number of messages by 1.
+    count--
+    println("You have $count unread messages.")
+}
